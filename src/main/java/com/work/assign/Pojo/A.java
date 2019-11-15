a74f86ee484c53de1481b0dd700029c37fe05a6d
Piyush <piyushjainwork01@gmail.com>
2019-11-15 19:12:17 +0530

    added pizza

--- src/main/java/com/work/assign/Pojo/A.java
+++ src/main/java/com/work/assign/Pojo/A.java
@@ -6,6 +6,10 @@ public class A {
 	public void a()
 	{
 		System.out.println("Piyush");
+		System.out.println("Eat Burger");
+		System.out.println("Eat Fries");
+		System.out.println("Eat Paneer");
+		
 	}
 
 }
